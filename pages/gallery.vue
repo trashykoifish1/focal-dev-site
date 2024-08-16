6e8f69ebdbcacde9e63bc23ed76181a9444f686f
trashykoifish1 <ntran41@asu.edu>
2024-08-16 00:28:28 -0700

    Fixed the lightbox function for gallery

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -21,7 +21,38 @@
       </div>
     </div>
 
-    <!-- Lightbox (unchanged) -->
+    <!-- Lightbox -->
+    <Transition name="fade">
+      <div
+        v-if="lightboxOpen"
+        class="fixed inset-0 bg-black bg-opacity-90 z-50 flex items-center justify-center"
+        @click="closeLightbox"
+      >
+        <button
+          @click="closeLightbox"
+          class="absolute top-4 right-4 text-cream text-2xl"
+        >
+          <XMarkIcon class="h-8 w-8" />
+        </button>
+        <button
+          @click.stop="prevImage"
+          class="absolute left-4 text-cream text-4xl"
+        >
+          <ChevronLeftIcon class="h-12 w-12" />
+        </button>
+        <button
+          @click.stop="nextImage"
+          class="absolute right-4 text-cream text-4xl"
+        >
+          <ChevronRightIcon class="h-12 w-12" />
+        </button>
+        <img
+          :src="imageUrls[currentImageIndex]"
+          :alt="`Gallery image ${currentImageIndex + 1}`"
+          class="max-h-[90vh] max-w-[90vw] object-contain"
+        />
+      </div>
+    </Transition>
   </div>
 </template>
 
@@ -35,7 +66,7 @@ import {
 } from "@heroicons/vue/24/solid";
 import { galleryImages } from "~/utils/galleryImages";
 
-const baseUrl = "/focal-dev-site"; // Add this line
+const baseUrl = "/focal-dev-site"; // You might want to make this dynamic based on environment
 
 const imageUrls = computed(() =>
   galleryImages.map((image) => `${baseUrl}/pictures/${image}`)
